60967c11c8631704df1527614bee0407ee3aef0f
arul <arul17102003@gmail.com>
2025-03-09 19:54:31 +0530

    new commit

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ import express from "express";
 const app = express();
 const port = 6969;
 
-app.get('/', (req,res)=> {
+app.get('/', (req,res) => {
     res.json({msg: `Hello STudents:${port}`});
-})
+});
+
+// crud functionality for movies
+
+// for getting the movies
+app.get("/movies", () => {
+
+});
+
+// for adding new movies
+app.post("/movies", () => {
+
+});
+
+
+//for updating movies
+app.put("/movies/:id", () => {
+
+});
+
+// deleting a movie
+app.delete("/movies/:id", () => {
+
+});
+
 
 app.listen(port, () => {
     console.log(`The server is running at http://127.0.0.1:${port}`);
-})
+});
